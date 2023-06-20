fcd85ed9e60b4b8733f6b7cd92891ab8d209f91d
diana120500 <diana.martos@inslapineda.cat>
2023-06-20 15:40:55 +0200

    Diana

---
+++ Beginners 05 Windows Forms and Event Handlers/Beginners 05 Windows Forms and Event Handlers/Operation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginners_05_Windows_Forms_and_Event_Handlers
+{
+    public class Operation
+    {
+        public string LeftSide {get;set;}
+        public string RightSide { get; set;}
+
+        //El tipo de operación a llevar a cabo
+        public OperationType OperationType { get; set;}
+
+        public Operation InnerOperation { get; set;}
+
+        //Constructor
+        public Operation()
+        {
+            this.LeftSide = string.Empty;
+            this.RightSide = string.Empty;
+        }
+    }
+}
---
+++ Beginners 05 Windows Forms and Event Handlers/Beginners 05 Windows Forms and Event Handlers/OperationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginners_05_Windows_Forms_and_Event_Handlers
+{
+    public enum OperationType
+    {
+        Add,
+        Minus,
+        Divide,
+        Multiply
+    }
+}
