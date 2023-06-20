25d7b3a3541a2b9b9d2b74c09dff2eb58e635be5
Diana <113168737+code-diana@users.noreply.github.com>
2023-06-20 15:53:14 +0200

    Delete Beginners 03 - While loops and Complex logic directory

--- Beginners 03 - While loops and Complex logic/Beginners 03 - While loops and Complex logic/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Beginners_03___While_loops_and_Complex_logic;
-
-//Crea una nueva instancia de la clase Number Guesser
-NumberGuesser numberGuesser = new NumberGuesser();  //se puede sustituir por var y cogerá el valor de new [...]
-numberGuesser.InformUser();
-numberGuesser.DiscoverNumber();
-numberGuesser.AnnounceResults();
